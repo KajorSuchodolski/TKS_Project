19b9f1f1fbe46db03756d851ba40ec7cdcc5e93d
229854 Ciszewski Tomasz <229854@edu.p.lodz.pl>
2022-03-23 00:02:32 +0100

    Added CostumeRepositoryAdapter, mapper

---
+++ repositories-adapters/src/main/java/org/ias/tks/appports/repoadapters/adapters/CostumeRepositoryAdapter.java
@@ -0,0 +1,71 @@
+package org.ias.tks.appports.repoadapters.adapters;
+
+import org.ias.tks.appcore.domainmodel.model.costume.Costume;
+import org.ias.tks.appports.infrastructure.costume.CreateCostumePort;
+import org.ias.tks.appports.infrastructure.costume.GetCostumePort;
+import org.ias.tks.appports.infrastructure.costume.RemoveCostumePort;
+import org.ias.tks.appports.infrastructure.costume.UpdateCostumePort;
+import org.ias.tks.appports.repoadapters.entities.costume.CostumeSizeEnt;
+import org.ias.tks.appports.repoadapters.entities.costume.ForWhomEnt;
+import org.ias.tks.appports.repoadapters.mappers.CostumeMapper;
+import org.ias.tks.appports.repoadapters.repositories.CostumeRepository;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.UUID;
+
+public class CostumeRepositoryAdapter implements CreateCostumePort, GetCostumePort, RemoveCostumePort, UpdateCostumePort {
+
+    @Inject
+    private CostumeRepository costumeRepository;
+
+    @Inject
+    private CostumeMapper costumeMapper;
+
+    @Override
+    public void addCostume(Costume costume) {
+        costumeRepository.addCostume(costumeMapper.mapToCostumeEntity(costume));
+    }
+
+    @Override
+    public Costume getCostumeById(UUID id) {
+        return costumeMapper.mapToCostume(costumeRepository.getCostumeById(id));
+    }
+
+    @Override
+    public List<Costume> getAll() {
+        return costumeMapper.mapToCostumeList(costumeRepository.getAll());
+    }
+
+    @Override
+    public List<Costume> getAllByRentStatus(boolean flag) {
+        return costumeMapper.mapToCostumeList(costumeRepository.getAllByRentStatus(flag));
+    }
+
+    @Override
+    public List<Costume> getAllCostumesByAge(String forWhom) {
+        return costumeMapper.mapToCostumeList(costumeRepository.getAllCostumesByAge(ForWhomEnt.valueOf(forWhom)));
+    }
+
+    @Override
+    public List<Costume> getAllCostumesByParams(String age, String size) {
+        return costumeMapper.mapToCostumeList(costumeRepository.getAllCostumesByParams(ForWhomEnt.valueOf(age), CostumeSizeEnt.valueOf(size)));
+    }
+
+    @Override
+    public List<Costume> searchAllCostumesByName(String name) {
+        return costumeMapper.mapToCostumeList(costumeRepository.searchCostumesByName(name));
+    }
+
+    @Override
+    public void removeCostume(UUID id) {
+        costumeRepository.removeCostume(id);
+    }
+
+    @Override
+    public void updateCostume(UUID id, Costume costume) {
+       costumeRepository.updateCostume(id, costumeMapper.mapToCostumeEntity(costume));
+    }
+
+
+}
---
+++ repositories-adapters/src/main/java/org/ias/tks/appports/repoadapters/mappers/CostumeMapper.java
@@ -0,0 +1,54 @@
+package org.ias.tks.appports.repoadapters.mappers;
+
+import org.ias.tks.appcore.domainmodel.model.costume.Costume;
+import org.ias.tks.appcore.domainmodel.model.costume.CostumeSize;
+import org.ias.tks.appcore.domainmodel.model.costume.ForWhom;
+import org.ias.tks.appcore.domainmodel.model.user.User;
+import org.ias.tks.appports.repoadapters.entities.costume.CostumeEntity;
+import org.ias.tks.appports.repoadapters.entities.costume.CostumeSizeEnt;
+import org.ias.tks.appports.repoadapters.entities.costume.ForWhomEnt;
+import org.ias.tks.appports.repoadapters.entities.user.UserEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CostumeMapper {
+
+    public CostumeEntity mapToCostumeEntity(Costume costume) {
+
+        return new CostumeEntity(
+                costume.getName(),
+                CostumeSizeEnt.valueOf(costume.getCostumeSize().toString()),
+                ForWhomEnt.valueOf(costume.getForWhom().toString()),
+                costume.getPrice());
+    }
+
+    public Costume mapToCostume(CostumeEntity costumeEntity) {
+        return new Costume(
+                costumeEntity.getName(),
+                CostumeSize.valueOf(costumeEntity.getCostumeSize().toString()),
+                ForWhom.valueOf(costumeEntity.getForWhom().toString()),
+                costumeEntity.getPrice());
+    }
+
+
+    public List<Costume> mapToCostumeList(List<CostumeEntity> listEntity) {
+        List<Costume> list = new ArrayList<>();
+
+        for (CostumeEntity costumeEntity : listEntity) {
+            list.add(mapToCostume(costumeEntity));
+        }
+        return list;
+    }
+
+    public List<CostumeEntity> mapToCostumeEntityList(List<Costume> list) {
+        List<CostumeEntity> listEntity = new ArrayList<>();
+        for (Costume costume : list) {
+            listEntity.add(mapToCostumeEntity(costume));
+        }
+        return listEntity;
+    }
+
+}
+
+
