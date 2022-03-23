752db78c48901b50afad7e63a482cada0dd417a7
229854 Ciszewski Tomasz <229854@edu.p.lodz.pl>
2022-03-23 12:09:15 +0100

    Added rest-adapter module, implementation of UseCase

--- rest-adapters/src/mappers/CostumeMapper.java
+++ rest-adapters/src/mappers/CostumeMapper.java
@@ -4,13 +4,13 @@ import dto.CostumeDto;
 import org.ias.tks.appcore.domainmodel.model.costume.Costume;
 
 public class CostumeMapper {
-
-    CostumeDto mapToCostumeDto(Costume costume){
-
-    }
-
-    Costume mapToCostume(CostumeDto costumeDto){
-
-    }
+//
+//    CostumeDto mapToCostumeDto(Costume costume){
+//
+//    }
+//
+//    Costume mapToCostume(CostumeDto costumeDto){
+//
+//    }
 
 }
