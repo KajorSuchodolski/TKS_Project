7fa87b17900099eead24183ef9678425fee53934
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-04-26 19:59:29 +0200

    Working payara micro tests

--- rest-adapters/src/test/java/org/ias/tks/restadapters/tests/CostumeRestTest.java
+++ rest-adapters/src/test/java/org/ias/tks/restadapters/tests/CostumeRestTest.java
@@ -3,12 +3,6 @@ package org.ias.tks.restadapters.tests;
 import lombok.extern.slf4j.Slf4j;
 import org.ias.tks.restadapters.dto.costume.CostumeDTO;
 import org.junit.jupiter.api.*;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -16,125 +10,108 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-import org.testcontainers.utility.MountableFile;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Testcontainers
 @Slf4j
 public class CostumeRestTest {
 
     private static String path;
     private static Client restClient;
-    private static WebTarget restTarget;
 
-    private static MountableFile warFile = MountableFile.forHostPath(Paths.get("target/rest-adapters-1.0-SNAPSHOT.war").toAbsolutePath(), 0777);
-
-    @Container
-    public static GenericContainer restService = new GenericContainer("payara/micro:latest")
-            .withExposedPorts(8080)
-            .withCopyFileToContainer(warFile, "/opt/payara/deployments/rest-adapters-1.0-SNAPSHOT.war")
-            .waitingFor(Wait.forLogMessage(".* Payara Micro .* ready in .*\\s", 1))
-            .withCommand("--deploy /opt/payara/deployments/rest-adapters-1.0-SNAPSHOT.war")
-            .withLogConsumer(new Slf4jLogConsumer(log));
-
-
-    @BeforeAll
-    public static void config() {
-        path = "http://localhost:8080/rest-adapters-1.0-SNAPSHOT/costume"; // port do zmiany!!!
-        restClient = ClientBuilder.newClient();
-        restTarget = restClient.target(path);
+    @BeforeEach
+    public void config() {
+        path = "http://localhost:8080/rest-adapters-1.0-SNAPSHOT/costume";
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+        restClient = clientBuilder.build();
     }
 
     @Test
     public void getAllTest() {
-        System.out.println(restService.getExposedPorts());
-        System.out.println(restService.getHost());
-        while(true) {
 
-        }
-//        CostumeDTO[] costumes = restClient.target(path)
+        CostumeDTO[] costumes = restClient.target(path)
+                .request(MediaType.APPLICATION_JSON)
+                .get(CostumeDTO[].class);
+
+        System.out.println(costumes[0].getName());
+    }
+
+//    @Test
+//    public void getAllRentedTest() {
+//        CostumeDTO[] costumes = restClient.target(path + "/all-rented")
 //                .request(MediaType.APPLICATION_JSON)
 //                .get(CostumeDTO[].class);
-
+//
 //        System.out.println(costumes);
-    }
-
-    @Test
-    public void getAllRentedTest() {
-        CostumeDTO[] costumes = restClient.target(path + "/all-rented")
-                .request(MediaType.APPLICATION_JSON)
-                .get(CostumeDTO[].class);
-
-        System.out.println(costumes);
-    }
-
-    @Test
-    public void getAllAvailableTest() {
-        CostumeDTO[] costumes = restClient.target(path + "/all-available")
-                .request(MediaType.APPLICATION_JSON)
-                .get(CostumeDTO[].class);
-
-        System.out.println(costumes);
-    }
-
-    @Test
-    public void getAllByAge() {
-        CostumeDTO[] costumes = restClient.target(path + "/all-by-age")
-                .request(MediaType.APPLICATION_JSON)
-                .get(CostumeDTO[].class);
-
-        System.out.println(Arrays.toString(costumes));
-    }
-
-    @Test
-    public void getByIdTest() {
-        CostumeDTO costume = restClient.target(path + "/id")
-                .request(MediaType.APPLICATION_JSON)
-                .get(CostumeDTO.class);
-
-        System.out.println(costume);
-    }
-
-    @Test
-    public void searchByNameTest() {
-        CostumeDTO[] costumes = restClient.target(path + "/search-by-name")
-                .queryParam("name","Furry")
-                .request(MediaType.APPLICATION_JSON)
-                .get(CostumeDTO[].class);
-
-        System.out.println(Arrays.toString(costumes));
-    }
-
-    @Test
-    public void costumesByParamsTest() {
-
-    }
-
-
-
-
-    @Test
-    public void addCostumeTest() {
-        String newCostume = "{\n" +
-                " \"costumeSize\" : S, \n" +
-                " \"name\": \"Fairy\",\n" +
-                " \"price\": 100, \n" +
-                " \"forWhom\": GIRLS\n" + "}";
-
-//        Assertions.assertEquals(200, responsePost);
-    }
-
-    @Test
-    public void updateCostumeTest() {
-
-    }
-
-    @Test
-    public void deleteCostumeTest() {
-
-    }
+//    }
+//
+//    @Test
+//    public void getAllAvailableTest() {
+//        CostumeDTO[] costumes = restClient.target(path + "/all-available")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(CostumeDTO[].class);
+//
+//        System.out.println(costumes);
+//    }
+//
+//    @Test
+//    public void getAllByAge() {
+//        CostumeDTO[] costumes = restClient.target(path + "/all-by-age")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(CostumeDTO[].class);
+//
+//        System.out.println(Arrays.toString(costumes));
+//    }
+//
+//    @Test
+//    public void getByIdTest() {
+//        CostumeDTO costume = restClient.target(path + "/id")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(CostumeDTO.class);
+//
+//        System.out.println(costume);
+//    }
+//
+//    @Test
+//    public void searchByNameTest() {
+//        CostumeDTO[] costumes = restClient.target(path + "/search-by-name")
+//                .queryParam("name","Furry")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(CostumeDTO[].class);
+//
+//        System.out.println(Arrays.toString(costumes));
+//    }
+//
+//    @Test
+//    public void costumesByParamsTest() {
+//
+//    }
+//
+//
+//
+//
+//    @Test
+//    public void addCostumeTest() {
+//        String newCostume = "{\n" +
+//                " \"costumeSize\" : S, \n" +
+//                " \"name\": \"Fairy\",\n" +
+//                " \"price\": 100, \n" +
+//                " \"forWhom\": GIRLS\n" + "}";
+//
+////        Assertions.assertEquals(200, responsePost);
+//    }
+//
+//    @Test
+//    public void updateCostumeTest() {
+//
+//    }
+//
+//    @Test
+//    public void deleteCostumeTest() {
+//
+//    }
 }
--- rest-adapters/src/test/java/org/ias/tks/restadapters/tests/UserRestTest.java
+++ rest-adapters/src/test/java/org/ias/tks/restadapters/tests/UserRestTest.java
@@ -1,143 +1,138 @@
-package org.ias.tks.restadapters.tests;
-
-import org.ias.tks.restadapters.dto.user.UserOutputDto;
-import org.junit.jupiter.api.*;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import lombok.extern.slf4j.Slf4j;
-
-import java.nio.file.Path;
-import java.util.Arrays;
-
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Testcontainers
-@Slf4j
-public class UserRestTest {
-
-    private static String path;
-    private static Client restClient;
-    private static WebTarget restTarget;
-
-
-    @Container
-    public static GenericContainer restService = new GenericContainer(
-            new ImageFromDockerfile()
-                    .withDockerfileFromBuilder(builder
-                            -> builder.from("payara/server-full")
-                                        .copy("rest-adapters-1.0-SNAPSHOT.war", "/opt/payara/deployments")
-                                        .build())
-                    .withFileFromPath("rest-adapters", Path.of("target", "rest-adapters-1.0-SNAPSHOT.war"))
-            )
-            .withExposedPorts(8080, 4848)
-            .waitingFor(Wait.forHttp("/rest-adapters-1.0-SNAPSHOT").forPort(8080).forStatusCode(200))
-            .withLogConsumer(new Slf4jLogConsumer(log));
-
-    @BeforeAll
-    public static void config() {
-        path = "http://localhost:" + restService.getMappedPort(8080) + "/rest-adapters-1.0-SNAPSHOT/user";
-        System.out.println(path);
-        restClient = ClientBuilder.newClient();
-        restTarget = restClient.target(path);
-    }
-
-    @Test
-    public void getAllTest() {
-        UserOutputDto[] users = restClient.target(path)
-                .request(MediaType.APPLICATION_JSON)
-                .get(UserOutputDto[].class);
-
-        System.out.println(users);
-
-    }
-
-    @Test
-    public void getByLoginTest() {
-        UserOutputDto foundUser = restClient.target(path + "/get-by-login")
-                .queryParam("login","KajorSuchodolski")
-                .request(MediaType.APPLICATION_JSON)
-                .get(UserOutputDto.class);
-
-        System.out.println(foundUser);
-    }
-
-    @Test
-    public void addUserTest() {
-        String newUser = "{\n" +
-                " \"firstName\" : \"Zbigniew\", \n" +
-                " \"lastName\": \"Kowalski\",\n" +
-                " \"login\": \"kowalski456\", \n" +
-                " \"password\": \"kowal123\" \n" + "}";
-
-        Response responsePost = restClient.target(path)
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.json(newUser));
-
-        Assertions.assertEquals(200, responsePost);
-    }
-
-    @Test
-    public void getUserByIdTest() {
-        UserOutputDto foundUser = restClient.target(path + "/get-by-id")
-//                .queryParam("id", "KajorSuchodolski")
-                .request(MediaType.APPLICATION_JSON)
-                .get(UserOutputDto.class);
-        System.out.println(foundUser);
-    }
-
-    @Test
-    public void searchUserByLoginTest() {
-        UserOutputDto[] foundUsers = restClient.target(path + "/search-by-login")
-                .queryParam("login","Radek")
-                .request(MediaType.APPLICATION_JSON)
-                .get(UserOutputDto[].class);
-
-        System.out.println(Arrays.toString(foundUsers));
-    }
-
-//    @Order(6)
+//package org.ias.tks.restadapters.tests;
+//
+//import org.ias.tks.restadapters.dto.user.UserOutputDto;
+//import org.junit.jupiter.api.*;
+//
+//
+//
+//import javax.ws.rs.client.Client;
+//import javax.ws.rs.client.ClientBuilder;
+//import javax.ws.rs.client.Entity;
+//import javax.ws.rs.client.WebTarget;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import lombok.extern.slf4j.Slf4j;
+//
+//import java.nio.file.Path;
+//import java.util.Arrays;
+//
+//
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@Testcontainers
+//@Slf4j
+//public class UserRestTest {
+//
+//    private static String path;
+//    private static Client restClient;
+//    private static WebTarget restTarget;
+//
+//
+//    @Container
+//    public static GenericContainer restService = new GenericContainer(
+//            new ImageFromDockerfile()
+//                    .withDockerfileFromBuilder(builder
+//                            -> builder.from("payara/server-full")
+//                                        .copy("rest-adapters-1.0-SNAPSHOT.war", "/opt/payara/deployments")
+//                                        .build())
+//                    .withFileFromPath("rest-adapters", Path.of("target", "rest-adapters-1.0-SNAPSHOT.war"))
+//            )
+//            .withExposedPorts(8080, 4848)
+//            .waitingFor(Wait.forHttp("/rest-adapters-1.0-SNAPSHOT").forPort(8080).forStatusCode(200))
+//            .withLogConsumer(new Slf4jLogConsumer(log));
+//
+//    @BeforeAll
+//    public static void config() {
+//        path = "http://localhost:" + restService.getMappedPort(8080) + "/rest-adapters-1.0-SNAPSHOT/user";
+//        System.out.println(path);
+//        restClient = ClientBuilder.newClient();
+//        restTarget = restClient.target(path);
+//    }
+//
 //    @Test
-//    public void uptadeUserTest() {
+//    public void getAllTest() {
+//        UserOutputDto[] users = restClient.target(path)
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(UserOutputDto[].class);
+//
+//        System.out.println(users);
+//
+//    }
+//
+//    @Test
+//    public void getByLoginTest() {
+//        UserOutputDto foundUser = restClient.target(path + "/get-by-login")
+//                .queryParam("login","KajorSuchodolski")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(UserOutputDto.class);
+//
+//        System.out.println(foundUser);
+//    }
+//
+//    @Test
+//    public void addUserTest() {
 //        String newUser = "{\n" +
 //                " \"firstName\" : \"Zbigniew\", \n" +
 //                " \"lastName\": \"Kowalski\",\n" +
 //                " \"login\": \"kowalski456\", \n" +
 //                " \"password\": \"kowal123\" \n" + "}";
-
-//        Response response = restClient.target(path + "/KajorSuchodolski/update")
+//
+//        Response responsePost = restClient.target(path)
 //                .request(MediaType.APPLICATION_JSON)
-//                .put();
+//                .post(Entity.json(newUser));
+//
+//        Assertions.assertEquals(200, responsePost);
 //    }
-
-    @Test
-    public void activateUserTest() {
-        Response response = restClient.target(path + "/KajorSuchodolski/activate")
-                .request(MediaType.APPLICATION_JSON)
-                .put(Entity.json(""));
-
-        Assertions.assertEquals(200, response);
-    }
-
-    @Test
-    public void deactivateUserTest() {
-        Response response = restClient.target(path + "/KajorSuchodolski/deactivate")
-                .request(MediaType.APPLICATION_JSON)
-                .put(Entity.json(""));
-
-        Assertions.assertEquals(200, response);
-    }
-
-}
-
+//
+//    @Test
+//    public void getUserByIdTest() {
+//        UserOutputDto foundUser = restClient.target(path + "/get-by-id")
+////                .queryParam("id", "KajorSuchodolski")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(UserOutputDto.class);
+//        System.out.println(foundUser);
+//    }
+//
+//    @Test
+//    public void searchUserByLoginTest() {
+//        UserOutputDto[] foundUsers = restClient.target(path + "/search-by-login")
+//                .queryParam("login","Radek")
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(UserOutputDto[].class);
+//
+//        System.out.println(Arrays.toString(foundUsers));
+//    }
+//
+////    @Order(6)
+////    @Test
+////    public void uptadeUserTest() {
+////        String newUser = "{\n" +
+////                " \"firstName\" : \"Zbigniew\", \n" +
+////                " \"lastName\": \"Kowalski\",\n" +
+////                " \"login\": \"kowalski456\", \n" +
+////                " \"password\": \"kowal123\" \n" + "}";
+//
+////        Response response = restClient.target(path + "/KajorSuchodolski/update")
+////                .request(MediaType.APPLICATION_JSON)
+////                .put();
+////    }
+//
+//    @Test
+//    public void activateUserTest() {
+//        Response response = restClient.target(path + "/KajorSuchodolski/activate")
+//                .request(MediaType.APPLICATION_JSON)
+//                .put(Entity.json(""));
+//
+//        Assertions.assertEquals(200, response);
+//    }
+//
+//    @Test
+//    public void deactivateUserTest() {
+//        Response response = restClient.target(path + "/KajorSuchodolski/deactivate")
+//                .request(MediaType.APPLICATION_JSON)
+//                .put(Entity.json(""));
+//
+//        Assertions.assertEquals(200, response);
+//    }
+//
+//}
+//
