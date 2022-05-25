2893e0c7efbea5a51c796b81c247def41d8c7f5c
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-05-25 23:38:47 +0200

    Added Dockerfile

--- rent-service/user-interface/src/main/java/org/ias/tks/ui/model/costume/ForWhom.java
+++
@@ -1,5 +0,0 @@
-package org.ias.tks.ui.model.costume;
-
-public enum ForWhom {
-    MAN, WOMEN, BOYS, GIRLS
-}
--- rent-service/user-interface/src/main/java/org/ias/tks/ui/model/user/access_levels/AccessLevelType.java
+++
@@ -1,5 +0,0 @@
-package org.ias.tks.ui.model.user.access_levels;
-
-public enum AccessLevelType {
-    CLIENT, MANAGER, ADMINISTRATOR
-}
